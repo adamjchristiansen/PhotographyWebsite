e46e0084674c110baec84e59ef28db22982a2a70
Adam Christiansen <adam.j.christiansen@gmail.com>
2015-05-08 12:29:23 -0600

    Finished ch 7 Templating with Handlebars

--- app.js
+++ app.js
@@ -5,6 +5,42 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+function getWeatherData() {
+	console.log("in getWeatherData()");
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+			},
+		],
+	};
+}
+
+app.use(function(req, res, next) {
+	console.log("in app.use");
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	console.log(res.locals.partials.weather);
+	next();
+});
+
+app.get('/nursery-rhyme', function(req, res) {
+	res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function(req, res) {
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
+});
+
 app.get('/', function(req, res) {
     res.render('home');
 });
