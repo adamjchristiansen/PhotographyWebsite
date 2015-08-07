61de6d0cd981cc7cbb6cf1d8aba041a69b28037e
Adam Christiansen <adam.j.christiansen@gmail.com>
2015-08-07 16:27:14 -0600

    chapter 8
    adding some file upload stuff from chapter 8. Started the vacation pic
    uploading but didn't really test it to see if it was working so might
    not be working correctly

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+
 var express = require('express');
 var path = require('path');
 var fortune = require('./lib/fortune.js');
 var bodyParser = require('body-parser');
+var formidable = require('formidable');
 var app = express();
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 function getWeatherData() {
@@ -28,6 +31,20 @@ app.use(function(req, res, next) {
 	next();
 });
 
+//Get requests
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about', { fortune: fortune.getFortune() });
+});
+
+app.get('/newsletter', function(req, res) {
+	res.render('newsletter', {csrf: 'CSRF goes here'});
+});
+
 app.get('/nursery-rhyme', function(req, res) {
 	res.render('nursery-rhyme');
 });
@@ -41,14 +58,58 @@ app.get('/data/nursery-rhyme', function(req, res) {
 	});
 });
 
-app.get('/', function(req, res) {
-    res.render('home');
+app.get('/thank-you', function(req, res) {
+	res.render('thank-you');
 });
 
-app.get('/about', function(req, res) {
-    res.render('about', { fortune: fortune.getFortune() });
+app.get('/contest/vacation-photo', function(req, res) {
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(), month: now.getMonth()
+	});
 });
 
+
+//Post requests
+
+// app.post('/process', function(req, res) {
+// 	console.log('Form (from querystring): ' + req.query.form);
+// 	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+// 	console.log('Name (from visible form field): ' + req.body.name;
+// 	console.log('EMail (from visible form field): ' + req.body.email);
+// 	res.redirect(303, '/thank-you');
+// });
+
+app.post('/process', function(req, res) {
+	console.log('Form (from querystring): ' + req.query.form);
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Name (from visible form field): ' + req.body.name);
+	console.log('Email (from visible form field): ' + req.body.email);
+
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
+
+
+//Fall to these responses if others aren't hit first
 //custom 404 page
 app.use(function(req, res){
 	res.status(404);
